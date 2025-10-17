9c5ebc1e6708a5f2cf78a9f6c736c652354cd574
JuatoVuela <truningerjusto@gmail.com>
2025-10-17 20:16:58 -0300

    Clase PowerUP implementacion parcial.

--- src/Entidades/PowerUp/PowerUp.java
+++ src/Entidades/PowerUp/PowerUp.java
@@ -1,5 +1,35 @@
 package Entidades.PowerUp;
+import Entidades.Entidad;
 
-abstract class PowerUp {
-    
+abstract class PowerUp extends Entidad {
+    protected int puntaje;
+    protected float tiempoDeVida; // en segundos
+
+    public PowerUp(int puntaje, float tiempoDeVida) {
+        this.puntaje = puntaje;
+        this.tiempoDeVida = tiempoDeVida;
+    }
+
+    public int getPuntaje() {
+        return puntaje;
+    }
+
+    public float getTiempoDeVida() {
+        return tiempoDeVida;
+    }
+
+    public void setPuntaje(int puntaje) {
+        this.puntaje = puntaje;
+    }
+
+    public void setTiempoDeVida(float tiempoDeVida) {
+        this.tiempoDeVida = tiempoDeVida;
+    }
+
+    public abstract void afectar(Jugador jugador);
+
+    public abstract void afectar(Enemigo enemigo);
+
+    public abstract void afectar(PowerUp powerUp);
+
 }
