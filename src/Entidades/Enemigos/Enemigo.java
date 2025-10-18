a6f8f65ddd6ff99219bac95af60f42ad98655aec
JuatoVuela <truningerjusto@gmail.com>
2025-10-18 16:13:46 -0300

    Clase abstracta enemigo

--- src/Entidades/Enemigos/Enemigo.java
+++ src/Entidades/Enemigos/Enemigo.java
@@ -1,5 +1,25 @@
 package Entidades.Enemigos;
 
 abstract class Enemigo{
+    protected int vida;
+    protected EstadoEnemigo estado;
+
+    public Enemigo(int vida){
+        this.vida = vida;
+        this.estado = new EstadoEnemigo(this);
+    }   
+    public abstract void atacar(Enemigo enemigo);
+
+    public abstract void atacar(Jugador jugador);
+
+    public abstract void moverse();
+    
+    public abstract void recibirDisparo();
+
+    public abstract void crearPowerUp();
+
+    public abstract void setEstado(EstadoEnemigo estado);
+
+    public abstract EstadoEnemigo getEstado();
 
 } 
