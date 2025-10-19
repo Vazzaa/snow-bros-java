1c5c8ed79a29ee564a8468f0b70f065b74e314bf
Vazzano Federico <vazzano9@gmail.com>
2025-10-18 21:38:37 -0300

    agregue metodos de fabricaSkin

--- src/FabricaSkin.java
+++ src/FabricaSkin.java
@@ -1,5 +1,27 @@
-public class FabricaSkin {
+import Entidades.Skin;
 
-    
-    
+abstract class FabricaSkin {
+
+    abstract Skin crearSkinDemonioRojo();
+    abstract Skin crearSkinTrollAmarillo();
+    abstract Skin crearSkinCalabaza();
+    abstract Skin crearSkinFantasma();
+    abstract Skin crearSkinMoghera();
+    abstract Skin crearSkinKamakichi();
+    abstract Skin crearSkinPowerUpAzul();
+    abstract Skin crearSkinPowerUpRojo();
+    abstract Skin crearSkinPowerUpVerde();
+    abstract Skin crearSkinFruta();
+    abstract Skin crearSkinVidaExtra();
+    abstract Skin crearSkinSnowBro();
+    abstract Skin crearSkinPincho();
+    abstract Skin crearSkinEscalera();
+    abstract Skin crearSkinPared();
+    abstract Skin crearSkinSueloResbaladizo();
+    abstract Skin crearSkinPlatQuebradiza();
+    abstract Skin crearSkinPlatMovil();
+    abstract Skin crearSkinProyectilBomba();
+    abstract Skin crearSkinProyectilFuego();
+    abstract Skin crearSkinProyectilNieve();
+    abstract Skin crearSkinBolaDeNieve();
 }
