03a44252226667320f03200affa9cc5134eb24c2
Vazzano Federico <vazzano9@gmail.com>
2025-11-01 14:59:49 -0300

    pusheo por jajas

--- src/EstadoMovimiento/EstadoMovimientoEnemigo.java
+++ src/EstadoMovimiento/EstadoMovimientoEnemigo.java
@@ -1,7 +1,9 @@
 package EstadoMovimiento;
 
 import Entidades.Enemigos.Enemigo;
+import Juego.ColisionManagerEntidades;
 
 public interface EstadoMovimientoEnemigo {
+    ColisionManagerEntidades colisionManager = new ColisionManagerEntidades();
     public void moverse(Enemigo enemigo, int velocidad);
 }
